36a0bc4e2b0bcc77a1063629604ba0107ec86193
Muhammad Awais <awaiszahid684@gmail.com>
2025-10-24 12:35:30 +0500

    Refactor deployment script to improve readability

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,13 +1,12 @@
 def call(String imageName, String tag, int port = 8000) {
-    try {
-        sh """
-        # Stop and remove existing container if running
-        # docker ps -q --filter "ancestor=${imageName}:${tag}" | xargs -r docker stop
-        # docker ps -a -q --filter "ancestor=${imageName}:${tag}" | xargs -r docker rm
+    sh """
+    # Stop and remove existing container if running
+    # docker ps -q --filter "ancestor=${imageName}:${tag}" | xargs -r docker stop
+    # docker ps -a -q --filter "ancestor=${imageName}:${tag}" | xargs -r docker rm
 
-        # Run new container
-        docker run -d -p ${port}:${port} ${imageName}:${tag}
-        """
-        # echo "✅ Deployment successful for ${imageName}:${tag}"
-    }
+    # Run new container
+    docker run -d -p ${port}:${port} ${imageName}:${tag}
+    """
+
+    echo "✅ Deployment successful for ${imageName}:${tag}"
 }
